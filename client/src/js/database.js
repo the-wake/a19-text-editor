b3e578eac08d2d6e1345d72d475871751fb1d0bc
Ben Martin <bmartin2009@gmail.com>
2022-02-20 22:04:19 -0600

    Hacked a solution to IDB problems.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,12 +15,13 @@ const initdb = async () =>
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
+  // console.log(content); // content ends up being just the data, not the whole object.
   const jateDb = await openDB('jateDB', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, data: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('🚀 - data saved to the database');
 };
 
 // Add logic for a method that gets all the content from the database
@@ -31,14 +32,13 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log(result);
-  if (result.value) {
-    console.log('Data found.');
-    console.log(result.value);
-    return result;
-  } else {
+  // This whole thing seems like complete jank and I can't tell why I have to do it or how IDB works.
+  console.log('Result from reloat getDb:', result);
+  if (!result[0]) {
     console.log('No data found.');
-    return
+  } else {
+    console.log('Data found:', result[0].data);
+    return result[0].data;
   }
 };
 
